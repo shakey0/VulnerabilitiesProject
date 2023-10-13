0ab4c2759dad33be021e387bf951810c11eb062b
Andrew <shakeyshakespeare0@gmail.com>
2023-10-13 10:30:43 +0100

    Secret moved to environment

--- login_form/__init__.py
+++ login_form/__init__.py
@@ -5,7 +5,7 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='super_secret_key',
+        SECRET_KEY=os.environ.get('SECRET_KEY', 'default_fallback_key'),
         DATABASE=os.path.join(app.instance_path, 'login_form.sqlite'),
     )
 
